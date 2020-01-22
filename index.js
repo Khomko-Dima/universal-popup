f4058c86708607bd9ca42f1df82ceb81d1056365
Dima Khomko <khomko.dima@gmail.com>
2020-01-22 17:59:15 +0300

    refactor

--- index.js
+++ index.js
@@ -7,52 +7,39 @@ window.addEventListener('DOMContentLoaded', function() {
             modalMass = [...(document.querySelectorAll('.Modal'))], //все модальные окна
             open_modal = document.querySelectorAll('.open_modal'); //все кнопки вызова модального окна
     let modal = null;
-    const closeModal = (modal) => { //Закрытие модального окна
-        modal.querySelector('.closeModal').addEventListener('click', ()=>{
-            closeModalFunc(modal);
-        })
-        overlay.addEventListener('click', (e)=>{
-            if (!e.target.classList.contains('modal-overlay')) return; 
-            closeModalFunc(modal);
-        }); //закрытие при нажатии вне модального окна
+    const closeModal = (e) => { //Закрытие модального окна
+        e.preventDefault();
+        const target = e.target;
+        if (target.classList.contains('modal-overlay') || target.closest('.close-modal')) {
+            modal.classList.remove('active');
+            overlay.classList.remove('active');
+            html.classList.remove('modal-is-locked');
+            modal.style.display = 'none';
+            overlay.removeEventListener('click', closeModal);
+        }
     };
-    const closeModalFunc = (modal) => {
-        modal.classList.remove('active');
-        overlay.classList.remove('active');
-        html.classList.remove('modal-is-locked');
-    };
-    const shoyPopup = (modal, overlay) => {
+    const shoyPopup = (modal) => {
         modal.classList.add('active');
         overlay.classList.add('active');
         html.classList.add('modal-is-locked');
+        modal.style.display = 'flex';
     };
-    open_modal.forEach((el) => el.addEventListener('click', function(event) {
+    const creatorPopup = (val) => {
+        const atr = val.dataset; //все значения data атрибутов
+        const modalContent = modal.querySelector('.modal-content');
+        modalContent.innerHTML = `<h3>${atr.header}</h3>`
+    };
+    const openModal = (e) => {
         const dataAction = event.target.dataset.action; // дата атрибут элемента по каторому нажали
         modal = modalMass.filter(el => el.dataset.target == dataAction)[0];// получение модального окна со значением равному data-target
-        const ollModal = [];
-        shoyPopup(modal, overlay); // ф. открывает попап.
-
-        const modal_first = [modal.querySelector('.Modal_Head')];
-
-        switch(dataAction){
-            case 'first':
-                ollModal.push(...modal_first);
-                creatorPopup(event.target, ollModal); // ф. заполняет поля попап окна из data свойст кнопки.
-                break;   
-        };
-        closeModal(modal);
-    }));
-    
-    const creatorPopup = (val, ollModal) => {
-        const [popupHeader, popupContent, popupInput] = ollModal;
-        const atr = val.dataset; //все значения data атрибутов
-
-        try {
-            popupHeader.innerText = atr.header;
-            popupContent.innerHTML = atr.contentHtml;
-        	popupInput.value = atr.inputValue;
-		}catch (e) {}
-        
+        shoyPopup(modal); // ф. открывает попап.
+        if (dataAction === 'first') {
+            creatorPopup(event.target);
+        }
+        if (dataAction === 'second') {
+            creatorPopup(event.target)
+        }
+        overlay.addEventListener('click', closeModal);
     };
-
+    open_modal.forEach((el) => el.addEventListener('click', openModal));
 });
